295b15017a1f73f555b115cdb0c26aa4510e9edf
Jesùs Contreras <jesusdavidcontrerash@gmail.com>
2025-03-14 13:42:00 -0500

    version 1

---
+++ src/javapractice/JavaPractice.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javapractice;
+
+/**
+ *
+ * @author JesusContreras
+ */
+public class JavaPractice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        int a =5;
+        int b =5;
+        System.out.println(a+b);
+    }
+    
+}
